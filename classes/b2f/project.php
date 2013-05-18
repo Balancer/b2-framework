478e66aaf7717a6b707526483146997f3b94b17b
Balancer <balancer@balancer.ru>
2013-05-19 00:25:49 +0400

    Работаем.

--- classes/b2f/project.php
+++ classes/b2f/project.php
@@ -2,4 +2,5 @@
 
 class b2f_project
 {
+	function router() { return b2_load('b2f_router'); }
 }
--- classes/b2f/router.php
+++ classes/b2f/router.php
@@ -2,4 +2,9 @@
 
 class b2f_router
 {
+	function load_uri($uri)
+	{
+		$app = new \Slim\Slim();
+		var_dump($app);
+	}
 }

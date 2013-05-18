5daa49828ccac075d66320b52db30bb9f0a92010
Balancer <balancer@balancer.ru>
2013-05-19 01:39:44 +0400

    Работа над роутерами.

--- classes/b2f/project.php
+++ classes/b2f/project.php
@@ -1,6 +1,20 @@
 <?php
 
-class b2f_project
+class b2f_project extends b2_object
 {
-	function router() { return b2_load('b2f_router'); }
+	function router() { return $this->b2()->load('b2f_router'); }
+
+	function project_dir()
+	{
+		return dirname(dirname(__DIR__));
+	}
+
+	function file($file_relative_name)
+	{
+		$full_file_name = $this->project_dir().'/'.$file_relative_name;
+		if(!file_exists($full_file_name))
+			return blib_array::factory();
+
+		return blib_array::factory(explode("\n", file_get_contents($full_file_name)));
+	}
 }
--- classes/b2f/router.php
+++ classes/b2f/router.php
@@ -1,10 +1,32 @@
 <?php
 
-class b2f_router
+use Kunststube\Router\Router,
+    Kunststube\Router\Route;
+
+class b2f_router extends b2_object
 {
 	function load_uri($uri)
 	{
-		$app = new \Slim\Slim();
-		var_dump($app);
+//		$slim = new \Slim\Slim();
+		$router = new Router;
+		foreach($this->b2()->projects() as $project)
+		{
+			foreach($project->file('data/route.dat') as $route_rule)
+			{
+				echo "$route_rule<br/>\n";
+
+				if(preg_match('/^(GET|POST)\s+(\S+)\s+(\S+)$/', $route_rule, $m))
+					$router->add($m[2], ['class' => $m[3], 'action' => 'show'] /*,
+						function($router)
+						{
+//							echo "Found 
+							var_dump($router);
+						} */
+					);
+
+				$path = parse_url($uri, PHP_URL_PATH);
+				$router->route($path);
+			}
+		}
 	}
 }

4ff4e7adda7656206242045e6cda042b1270ce51
Balancer <balancer@balancer.ru>
2013-05-29 02:21:08 +0400

    Обновление до первой условно рабочей версии.

--- classes/b2f/router.php
+++ classes/b2f/router.php
@@ -5,28 +5,44 @@ use Kunststube\Router\Router,
 
 class b2f_router extends b2_object
 {
+	private $router = NULL;
+
 	function load_uri($uri)
 	{
 //		$slim = new \Slim\Slim();
-		$router = new Router;
-		foreach($this->b2()->projects() as $project)
+		if(!$this->router)
 		{
-			foreach($project->file('data/route.dat') as $route_rule)
+			$this->router = new Router;
+			foreach($this->b2()->projects() as $project)
 			{
-				echo "$route_rule<br/>\n";
+				foreach($project->file('data/route.dat') as $route_rule)
+				{
+//					echo "$route_rule<br/>\n";
 
-				if(preg_match('/^(GET|POST)\s+(\S+)\s+(\S+)$/', $route_rule, $m))
-					$router->add($m[2], ['class' => $m[3], 'action' => 'show'] /*,
-						function($router)
-						{
-//							echo "Found 
-							var_dump($router);
-						} */
-					);
-
-				$path = parse_url($uri, PHP_URL_PATH);
-				$router->route($path);
+					if(preg_match('/^(GET|POST)\s+(\S+)\s+(\S+)$/', $route_rule, $m))
+					{
+//						var_dump($m);
+						$this->router->add($m[2], array('class_name' => $m[3]));
+					}
+				}
 			}
+
+			$this->router->defaultCallback(function (Route $route) {
+				$disp = $route->dispatchValues();
+//				var_dump($route, $disp);
+				$x = $this->b2()->load($disp['class_name'], NULL);
+				return $x;
+			});
 		}
+
+		$path = parse_url($uri, PHP_URL_PATH);
+		try {
+			$object = $this->router->route($path);
+		} catch(Exception $e)
+		{
+			$object = NULL;
+		}
+
+		return $object;
 	}
 }

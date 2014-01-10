069cea1bbc0e360bc8b57aa93c7a910fa437ef5e
Balancer <balancer@balancer.ru>
2014-01-10 17:31:43 +0400

    Работа внутри других проектов.

--- init.php
+++ init.php
@@ -1,7 +1,12 @@
 <?php
 
-$GLOBALS['bors.composer.class_loader'] = require_once __DIR__.'/vendor/autoload.php';
+if(!defined('COMPOSER_ROOT'))
+	define('COMPOSER_ROOT', __DIR__);
 
-define('BORS_SITE', __DIR__);
+if(empty($GLOBALS['bors.composer.class_loader']))
+	$GLOBALS['bors.composer.class_loader'] = require_once COMPOSER_ROOT.'/vendor/autoload.php';
 
-require_once __DIR__.'/vendor/balancer/bors-core/init.php';
+if(!defined('BORS_SITE'))
+	define('BORS_SITE', __DIR__);
+
+require_once COMPOSER_ROOT.'/vendor/balancer/bors-core/init.php';

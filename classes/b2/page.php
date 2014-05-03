0bb5741d7b4ffe95022331e40501810e80f113ae
Balancer <balancer@balancer.ru>
2014-05-04 03:03:33 +0400

    Работа над b2-framework

--- classes/b2/page.php
+++ classes/b2/page.php
@@ -2,4 +2,10 @@
 
 class b2_page extends bors_page
 {
+	static function factory()
+	{
+		return b2::factory()->load(get_called_class(), NULL);
+	}
+
+	var $template = 'xfile:bootstrap3-noassets.tpl';
 }

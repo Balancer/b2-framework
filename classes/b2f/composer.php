3015c5ea5324acd3183d826fddc570cd17bc0e26
Balancer <balancer@balancer.ru>
2015-03-01 03:54:15 +0300

    ++

--- classes/b2f/composer.php
+++ classes/b2f/composer.php
@@ -4,4 +4,11 @@ class b2f_composer
 {
 	static function update() { echo "b2f_composer::Update!\n"; }
 	static function install() { echo "b2f_composer::Install!\n"; }
+
+	public static function postAutoloadDump(Event $event)
+	{
+        $composer = $event->getComposer();
+        $io = $event->getIO();
+        $io->write('<info>post, post, post</info>');
+	}
 }
